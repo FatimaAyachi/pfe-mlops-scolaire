765aa0ef19528afcb8060feaa1ef686866b66924
Fatima Ayachi <ouladelayachifatima@gmail.com>
2026-06-26 00:54:53 +0100

    Remove sensitive configuration and prepare API for public release

--- api/main.py
+++ api/main.py
@@ -1,17 +1,35 @@
 import pandas as pd
 from fastapi import FastAPI
 from pydantic import BaseModel
-import mlflow.pyfunc
 import uvicorn
+import joblib
+import os
+from fastapi.middleware.cors import CORSMiddleware
+import sentry_sdk
+
+sentry_sdk.init(
+    # Remplacez par votre propre DSN Sentry
+    dsn="your_sentry_dsn_here",
+    send_default_pii=True,
+)
 
 app = FastAPI()
 
-
-model = mlflow.pyfunc.load_model(
-    "models:/LREG_Production_Model_VF_REG/Production"
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
-print(f"Le model a ete charger la version  \n {model.metadata}")
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
+MODEL_PATH = os.path.join(BASE_DIR, "model.pkl")
+
+model = joblib.load(MODEL_PATH)
+
+print("True")
 
 class Student_input(BaseModel):
     age: int
@@ -29,9 +47,12 @@ class Student_input(BaseModel):
     traveltime: int
     freetime: int
     famrel: int
-@app.post("/")
+
+@app.get("/")
 async def root():
     return {"message": "API FastAPI fonctionne"}
+
+
 @app.post("/predict")
 async def predection_endpoint(data: Student_input):
     
@@ -45,7 +66,7 @@ async def predection_endpoint(data: Student_input):
 
 if __name__ ==  '__main__':
     uvicorn.run(
-        app,
-        host="127.0.0.1",
-        port = 8080
+        "main:app",
+        host="0.0.0.0",
+        port = 7860
     )
